722f836dda37e21a56129e7998f318dd17495394
aycan <aycann21@yahoo.com.tr>
2021-08-27 20:42:06 +0200

    2.day of RestAssured

--- src/test/java/ZippoTest2.java
+++ src/test/java/ZippoTest2.java
@@ -4,76 +4,7 @@ import org.testng.annotations.Test;
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 
-public class ZippoTest {
-
-    @Test
-    public void test(){
-        given()
-                .when()
-                .then()
-        ;
-    }
-    @Test
-    public void statusCodeTest(){
-        given()
-                .when()
-                .get("http://api.zippopotam.us/us/90210")
-
-                .then()
-
-                //.log().all()--> all respons
-                .log().body()
-                .statusCode(200)
-        ;
-
-    }
-    @Test
-    public void contentTypeTest(){
-        given()
-                .when()
-                .get("http://api.zippopotam.us/us/90210")
-
-                .then()
-                .log().body()
-                .contentType(ContentType.JSON)
-        ;
-
-    }
-    @Test
-    public void logTest(){
-        given()
-                .log().all()
-                .when()
-                .get("http://api.zippopotam.us/us/90210")
-
-                .then()
-                .log().body()
-
-        ;
-    }
-    @Test
-    public void checkStateInResponseBody (){
-        given()
-                .when()
-                .get("http://api.zippopotam.us/us/90210")
-
-                .then()
-                .log().body()
-                .body("country",equalTo("United States"))
-                .statusCode(200)
-        ;
-    }
-    @Test
-    public void bodyJsonPathTest (){
-        given()
-                .when()
-                .get("http://api.zippopotam.us/us/90210")
-                .then()
-                .log().body()
-                .body("places[0].state",equalTo("california"))
-                .statusCode(200)
-        ;
-    }
+public class ZippoTest2 {
     @Test
     public void bodyJsonPathTestHasItem() {
         given()
@@ -168,7 +99,7 @@ public class ZippoTest {
         }
     }
     //https://gorest.co.in/public/v1/users?page=1
-    @Test
+   @Test
     public void queryParamTest(){
         given()
 
@@ -180,9 +111,9 @@ public class ZippoTest {
                 .then()
                 .log().body()
                 .body("meta.pagination.page",equalTo(1))
-        ;
+                ;
 
-    }
+   }
     @Test
     public void queryParamTestCoklu(){
         for (int page = 1; page < 10; page++) {
@@ -203,5 +134,3 @@ public class ZippoTest {
     }
 
 }
-
-
